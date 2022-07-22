5df84c96a157597d92f5305757227cef8a14571c
tony760815 <tony760815@gmail.com>
2022-07-23 01:00:11 +0800

    docs: Create project

---
+++ LeetCodePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeetCodePractice
+//
+//  Created by HUNG-HSUN LIN on 2022/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
